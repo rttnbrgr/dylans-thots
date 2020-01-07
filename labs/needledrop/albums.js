3b830ce4f6d29b47ea2a11daa09c199f75267410
Nathan rothenberger <nathan.rothenberger@sprinklr.com>
2020-01-07 09:21:14 -0800

    Add data for needle drop proj

---
+++ labs/needledrop/albums.js
@@ -0,0 +1,251 @@
+const foo = [
+  {
+    title: "100 gecs - 1000 gecs",
+    link: "https://www.youtube.com/watch?v=IhxPV..."
+  },
+
+  {
+    title: "Alex Cameron - Miami Memory",
+    link: "https://youtu.be/cRO0BuhzDsw"
+  },
+
+  {
+    title: "Ana Frango Elétrico - Little Electric Chicken Heart",
+    link: "https://youtu.be/Ufd4o67Ze6w"
+  },
+
+  {
+    title: "Anamanaguchi - [USA]",
+    link: "https://youtu.be/Ue7WGgA_2_Q"
+  },
+
+  {
+    title: "Ariana Grande - thank u, next",
+    link: "https://www.youtube.com/watch?v=VWjFq..."
+  },
+
+  {
+    title: "Beyoncé - HOMECOMING: The Live Album",
+    link: "https://youtu.be/FGILPXMZHXw"
+  },
+
+  {
+    title: "Billie Eilish - When We All Fall Asleep, Where Do We Go?",
+    link: "https://www.youtube.com/watch?v=L9fWq..."
+  },
+
+  {
+    title: "Billy Woods & Kenny Segal - Hiding Places",
+    link: "https://youtu.be/_EbckCohk7s"
+  },
+
+  {
+    title: "black midi - Schlagenheim",
+    link: "https://youtu.be/vqmCLg0zoyo"
+  },
+
+  {
+    title: "Blu & Oh No - A Long Red Hot Los Angeles Summer Night",
+    link: "https://www.youtube.com/watch?v=jV_Ij..."
+  },
+
+  {
+    title: "BROCKHAMPTON - GINGER",
+    link: "https://youtu.be/B7oeiUvMqXA"
+  },
+
+  {
+    title: "Caroline Polachek - Pang",
+    link: "https://youtu.be/ieXeM1YpqDg"
+  },
+
+  {
+    title: "Charli XCX - Charli",
+    link: "https://youtu.be/nA-_v2cJges"
+  },
+
+  {
+    title: "clipping. - There Existed an Addiction to Blood",
+    link: "https://youtu.be/jtPlfmhPnNg"
+  },
+
+  {
+    title: "Danny Brown - uknowhatimsayin¿",
+    link: "https://youtu.be/V2ZTJ8oD4TQ"
+  },
+
+  {
+    title: "Denzel Curry - ZUU",
+    link: "https://youtu.be/SAnc49e-G08"
+  },
+
+  {
+    title: "Dorian Electra - Flamboyant",
+    link: "https://youtu.be/xg9JtJJ11nc"
+  },
+
+  {
+    title: "Flume - Hi This Is Flume",
+    link: "https://youtu.be/5_ygpHqUm54"
+  },
+
+  {
+    title: "Freddie Gibbs & Madlib - Bandana",
+    link: "https://youtu.be/zN-S-2NIYLU"
+  },
+
+  {
+    title: "Hobo Johnson - The Fall of Hobo Johnson",
+    link: "https://youtu.be/Nm1tOpo5Pwo"
+  },
+
+  {
+    title: "JPEGMAFIA - All My Heroes Are Cornballs",
+    link: "https://youtu.be/ZGmQ9_ceFoY"
+  },
+
+  {
+    title: "King Gizzard & The Lizard Wizard - Infest the Rats’ Nest",
+    link: "https://www.youtube.com/watch?v=Puf8W..."
+  },
+
+  {
+    title: "Kirin J. Callinan - Return to Center",
+    link: "https://youtu.be/EbkAfmNLmwM"
+  },
+
+  {
+    title: "Lana Del Rey - Norman Fucking Rockwell!",
+    link: "https://youtu.be/9fewwAkSrCw"
+  },
+
+  {
+    title: "Lightning Bolt - Sonic Citadel",
+    link: "https://youtu.be/sZzwK3n_KEI"
+  },
+
+  {
+    title: "Lingua Ignota - Caligula",
+    link: "https://youtu.be/QOwE58DvVQ8"
+  },
+
+  {
+    title: "Little Simz - GREY Area",
+    link: "https://youtu.be/8bHjPSqqv-4"
+  },
+
+  {
+    title: "Liturgy - H.A.Q.Q.",
+    link: "https://youtu.be/NMsEzAg_E7o"
+  },
+
+  {
+    title: "Lizzo - Cuz I Love You",
+    link: "https://youtu.be/sli3C0KTDh8"
+  },
+
+  {
+    title: "Malibu Ken - Malibu Ken",
+    link: "https://www.youtube.com/watch?v=BFWxk..."
+  },
+
+  {
+    title: "Matana Roberts - COIN COIN Chapter Four: Memphis",
+    link: "https://www.youtube.com/watch?v=1J3s6..."
+  },
+
+  {
+    title: "Matmos - Plastic Anniversary",
+    link: "https://www.youtube.com/watch?v=XrNB3..."
+  },
+
+  {
+    title: "Megan Thee Stallion - Fever",
+    link: "https://youtu.be/AzbBTaF45cg"
+  },
+
+  {
+    title: "Otoboke Beaver - Itekoma Hits",
+    link: "https://youtu.be/4kggrlYjGo8"
+  },
+
+  {
+    title: "PUP - Morbid Stuff",
+    link: "https://youtu.be/3qjNDxkAv4w"
+  },
+
+  {
+    title: "Purple Mountains - Purple Mountains",
+    link: "https://youtu.be/Vw-Hh4kg9q4"
+  },
+
+  {
+    title: "Quelle Chris - Guns",
+    link: "https://youtu.be/zJ5A5CdlnTo"
+  },
+
+  {
+    title: "Rapsody - Eve",
+    link: "https://youtu.be/L_xu6Koreqs"
+  },
+
+  {
+    title: "Richard Dawson - 2020",
+    link: "https://youtu.be/fpATa8UJPh8"
+  },
+
+  {
+    title: "Slauson Malone - A Quiet Farwell, 2016-2018",
+    link: "https://youtu.be/Rccn_Hobk6s"
+  },
+
+  {
+    title: "slowthai - Nothing Great About Britain",
+    link: "https://youtu.be/Be70pP6C4ow"
+  },
+
+  {
+    title: "Sunn O))) - Pyroclasts",
+    link: "https://youtu.be/3mE4p3Ctlek"
+  },
+
+  {
+    title: "The Comet Is Coming - Trust in the Lifeforce of the Deep Mystery",
+    link: "https://www.youtube.com/watch?v=07CQa..."
+  },
+
+  {
+    title: "The Highwomen - The Highwomen",
+    link: "https://youtu.be/1Cwqhd2me2U"
+  },
+
+  {
+    title: "Tropical Fuck Storm - Braindrops",
+    link: "https://youtu.be/hItRO3zBdcw"
+  },
+
+  {
+    title: "Tyler, the Creator - IGOR",
+    link: "https://youtu.be/u37lG0BMFZk"
+  },
+
+  {
+    title: "Uboa - The Origin of My Depression",
+    link: "https://youtu.be/tIo6MOswfY8"
+  },
+
+  {
+    title: "Weyes Blood - Titanic Rising",
+    link: "https://youtu.be/ANqN835Gmro"
+  },
+
+  {
+    title: "Xiu Xiu - Girl with Basket of Fruit",
+    link: "https://www.youtube.com/watch?v=mW7m4..."
+  },
+
+  {
+    title: "Yazz Ahmed - Polyhymnia",
+    link: "https://youtu.be/P-A-j2rlsvo"
+  }
+]
