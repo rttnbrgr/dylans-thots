560a97d55097dd039999b84d01414bb2600c7b98
Nathan rothenberger <nathan.rothenberger@sprinklr.com>
2020-01-07 09:21:24 -0800

    Add test page

---
+++ pages/labs/needledrop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+cont NeedleDropTop50Page = props => {
+  const foo = 'foo';
+  return (
+    <div>
+      <h1>hi</h1>
+    </div>
+  )
+}
+
+export default NeedleDropTop50Page
