c7965105c4007b839b6589e195689aeb76658932
Nathan Rothenberger <nroth10@gmail.com>
2020-01-07 21:49:22 -0800

    Add test theme-ui && next steps

--- src/pages/labs/needledrop.js
+++ src/pages/labs/needledrop.js
@@ -1,3 +1,5 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
 import React from 'react';
 
 const NeedleDropTop50Page = props => {
@@ -5,8 +7,22 @@ const NeedleDropTop50Page = props => {
   return (
     <div>
       <h1>hi</h1>
+      <div
+        sx={{
+          color: "blue"
+        }}
+      >hi</div>
     </div>
   )
 }
+// @DONE
+// added rough content
+// @WORKING
+// add emotion
+// @NEXT
+// setup the grid
+// fake 50 array
+// iterate over
+// style for 4 wide
 
 export default NeedleDropTop50Page
