20938da58e8821e5f96d360969fa00e32ceaa193
Nathan Rothenberger <nroth10@gmail.com>
2020-01-07 22:08:43 -0800

    WIP

--- src/pages/labs/needledrop.js
+++ src/pages/labs/needledrop.js
@@ -1,6 +1,8 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
 import React from 'react';
+import { Styled } from 'theme-ui'
+import '../../labs/needledrop/reset.css';
 
 const NeedleDropTop50Page = props => {
   const foo = 'foo';
@@ -9,18 +11,38 @@ const NeedleDropTop50Page = props => {
       <h1>hi</h1>
       <div
         sx={{
-          color: "blue"
+          color: "blue",
+          display: 'grid',
+          gridTemplateColumns: 'repeat(4, 25vw)',
+          gridTemplateRows: 'repeat(1, 25vw)',
+          gridAutoRows: '25vw',
+          width: '100%',
+          'div': {
+            bg: 'pink'
+          },
         }}
-      >hi</div>
+      >
+      <div sx={{bg: 'blue'}} />
+      <div sx={{bg: 'green'}} />
+      <div sx={{bg: 'red'}} />
+      <div sx={{bg: 'orange'}} />
+      {new Array(50).fill('empty').map((x,j) => (
+        <div key={j}>
+        <Styled.h2>
+        {`${j+1}`}
+        </Styled.h2>
+        </div> 
+        ))}
+      </div>
     </div>
   )
 }
 // @DONE
 // added rough content
 // @WORKING
-// add emotion
-// @NEXT
 // setup the grid
+// @NEXT
+// add emotion
 // fake 50 array
 // iterate over
 // style for 4 wide
