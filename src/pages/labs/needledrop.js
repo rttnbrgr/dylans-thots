b32b266cc4f7617e42b1e786b1c62b8f023c5811
Nathan Rothenberger <nroth10@gmail.com>
2020-01-11 17:30:24 -0800

    WIP

--- src/pages/labs/needledrop.js
+++ src/pages/labs/needledrop.js
@@ -15,10 +15,11 @@ const NeedleDropTop50Page = props => {
       <div
         sx={{
           display: 'grid',
-          gridTemplateColumns: 'repeat(4, 25vw)',
-          gridTemplateRows: 'repeat(1, 25vw)',
-          gridAutoRows: '25vw',
+          gridTemplateColumns: ['repeat(2, calc(50vw - 5px))', 'repeat(3, calc(33.3333vw - 4px))', 'repeat(3, calc(33.3333vw - 4px))', 'repeat(4, 25vw)'],
+          // gridTemplateRows: 'repeat(1, 25vw)',
+          gridAutoRows: ['calc(50vw - 5px)', 'calc(33.3333vw - 4px)', 'calc(33.3333vw - 4px)', '25vw'],
           width: '100%',
+          gridGap: ['10px', '12px']
         }}
       >
       {DATA.map((x,j) => (
@@ -26,19 +27,36 @@ const NeedleDropTop50Page = props => {
           textDecoration: 'none',
           color: 'initial',
           display: 'flex',
+          padding: ['1rem', '1.25rem'],
           flexDirection: 'column',
-          alignItems: 'center',
+          alignItems: 'space-between',
           justifyContent: 'center',
+          // justifyContent: 'space-between',
           bg: () => `${j % 3 ? ( j % 2 ? 'red' : 'green' ) : 'blue'}`,
+          bg: ['pink', 'blue', 'yellow', 'green', 'red'],
           transform: 'scale(1)',
           transition: 'transform ease 0.3s',
+          
           '&:hover': {
             transform: 'scale(1.2)',
             zIndex: '99'
+          },
+
+          'h2': {
+            fontFamily: 'sans-serif',
+            fontSize: ['16px', '24px', '24px', '36px'],
+            fontWeight: 'normal',
+            color: 'white',
+            mb: '0',
+            lineHeight: 1.3,
+            '&.mb': {
+              mb: '1rem'
+            }
           }
+
         }}>
-            <Styled.h2>{`${j+1}`}</Styled.h2>
-            <Styled.p>{x.title}</Styled.p>
+            <Styled.h2 className="mb">{`${j+1}`}</Styled.h2>
+            <Styled.h2>{x.title}</Styled.h2>
         </a> 
       ))}
       </div>
